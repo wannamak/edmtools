cd391f5905ad115ad62c47b3bcd8f5237b7cf58d
wannamak <keith@wannamaker.org>
2026-09-05 20:08:27 -0400

    Upgrade script references to new protobuf

--- scripts/JpiRewrite.sh
+++ scripts/JpiRewrite.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CLASSPATH=lib/guava-18.0.jar
-CLASSPATH=$CLASSPATH:lib/proto-3.0.0.jar
+CLASSPATH=$CLASSPATH:lib/protobuf-java-3.21.12.jar
 CLASSPATH=$CLASSPATH:lib/args4j-2.32.jar
 CLASSPATH=$CLASSPATH:bin
 
